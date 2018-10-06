fd94a3350a32a3e64a354b024a0089b2f884759f
Pritish Chatterjee <42893363+pritish99@users.noreply.github.com>
2018-10-06 16:18:37 +0530

    Add files via upload

---
+++ Calculator.py
@@ -0,0 +1,25 @@
+def main():
+    print("Welcome to calculator")
+    print("For addition type add")
+    print("For subtraction type subtract")
+    print("For multiplication type multiply")
+    print("For division type divide")
+    user_input = input(" :")
+    num1= float(input("Enter a number:"))
+    num2= float(input("Enter another number:"))
+    if user_input == "add":
+        print(num1,"+",num2,"=",num1+num2)
+    elif user_input == "subtract":
+        print(num1,"-",num2,"=",num1-num2)
+    elif user_input == "multiply":
+        print(num1,"*",num2,"=",num1*num2)
+    elif user_input == "divide":
+        print(num1,"/",num2,"=",num1/num2)
+    else:
+        print("Incorrect input")
+    x= input("To restart please say yes or no to exit:")
+    if x == "yes":
+        main()
+    else:
+        exit
+main()
